f8bd7f68799a809ac88be9391603c541b9a318d9
Piumatti <francopiumatti2004@gmail.com>
2024-09-28 15:31:02 -0300

    finished shell

--- Shell/cat.c
+++ Shell/cat.c
@@ -9,8 +9,25 @@
 
 int main(int argc, char *argv[]){
 
-	printf(" %s \n",argv[0]);
+	char *filename = argv[1];
+	FILE *archivo = fopen(filename,"r");
+		
 	
+	if(archivo == NULL){
+		printf("Descripcion del error: %s\n",strerror(errno));
+		return 1;
+	}
+	
+	char linea[100];
+		
+	while(fgets(linea,sizeof(linea),archivo) != NULL){
+		printf("%s",linea);
+	}
+	
+	
+
+	
+	fclose(archivo);
 		
 	return 0;
 }
--- Shell/chmod.c
+++ Shell/chmod.c
@@ -8,16 +8,34 @@
 
 int main(int argc, char *argv[]){
 
-	char *permisos = strtok(argv[1]," ");
-	mode_t perm =  strtol(permisos,NULL,8);
-	int cheq_perm = perm;
-	int temp;
-	while(cheq_perm != 0){
-			
-	
+	char *permisos = strtok(argv[1]," "), *endptr;
+	mode_t perm =  strtol(permisos,&endptr,8);
+	if(endptr == permisos || *endptr != '\0'){
+		printf("Hay caracteres invalidos en el numero de permiso\n");
+		return 0;
 	}
 	
 	
+	int cheq_perm = perm;
+	
+	int temp, digits = 1;
+	while(cheq_perm != 0 && digits <=3){
+		temp = cheq_perm % 10;
+		if(temp >7 || temp <0){ //si no es un digito valido se termina
+			printf("Numero de permiso invalido\n");
+			return 0;
+			
+		} 
+		digits++;
+	
+		cheq_perm = cheq_perm / 10;
+	
+	}
+	
+	if(cheq_perm != 0){
+		printf("Numero de permiso invalido\n");
+		return 0;
+	}
 	
 	
 	
