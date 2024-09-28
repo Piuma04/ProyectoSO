01181eea65d7ad64077d57ff572e8b82733ec713
Piumatti <francopiumatti2004@gmail.com>
2024-09-28 14:51:18 -0300

    addind something

--- Shell/chmod.c
+++ Shell/chmod.c
@@ -8,7 +8,25 @@
 
 int main(int argc, char *argv[]){
 
-	printf(" %s \n",argv[0]);
+	char *permisos = strtok(argv[1]," ");
+	mode_t perm =  strtol(permisos,NULL,8);
+	int cheq_perm = perm;
+	int temp;
+	while(cheq_perm != 0){
+			
+	
+	}
+	
+	
+	
+	
+	
+	char *archivo = strtok(NULL," ");
+	
+	if(chmod(archivo,perm) != 0){
+		printf("Descripcion del error: %s\n",strerror(errno));
+	}
+	
 	
 		
 	return 0;
