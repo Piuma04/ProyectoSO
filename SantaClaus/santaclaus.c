c01aa87dc444bae0635c0a5182ebb222be0dcd3a
Piumatti <francopiumatti2004@gmail.com>
2024-09-30 16:27:35 -0300

    now santaclaus works properly

--- SantaClaus/santaclaus.c
+++ SantaClaus/santaclaus.c
@@ -9,74 +9,96 @@
 #define CANT_ELFO 3
 #define CANT_RENO 4
 
-sem_t despertar;
-sem_t otros_elfos;
+//usados para garantizar exclusion mutua con santa en la parte de ver si llenaron el grupo
 pthread_mutex_t renos = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t elfos = PTHREAD_MUTEX_INITIALIZER;
+
+//sriven para ver quien desperto a santa
+sem_t despertar; 
+sem_t despertar_reno; 
+sem_t despertar_elfo;
+
+
+sem_t otros_elfos; //limita la cantidad de elfos que pueden pedir ayuda
+
+
 sem_t elf_con_dif ;//contador
-sem_t esperando_santa_elf ;//hace que esperen a santa
 sem_t ren_vuelta_vacaciones; //contador
+
+sem_t esperando_santa_elf;//hace que esperen a santa
 sem_t esperando_en_cabaña; //hace que esperen a santa
 
 void *elfo(void *arg){
 	int dificultad_al_hacer_juguete;
-	for(int i = 0; i<2; i++){
-		dificultad_al_hacer_juguete = (int) (1); //hay un 10% de prob de necesitar ayuda
-		if(dificultad_al_hacer_juguete){  
+	for(int h = 0; h<2; h++){
+		dificultad_al_hacer_juguete = (int) (rand() % 10 + 1 ); //hay un 10% de prob de necesitar ayuda
+		printf("%d\n",dificultad_al_hacer_juguete);
+		
+		if(dificultad_al_hacer_juguete = 4){  
+			
 			printf("necesito ayuda!\n");
 			sem_wait(&otros_elfos);
 			printf("entre al grupo! \n");
+			
 			pthread_mutex_lock(&elfos);
-				if( sem_trywait(&elf_con_dif) != 0)
-					sem_post(&despertar); 	
+				if( sem_trywait(&elf_con_dif) != 0){
+					sem_post(&despertar_elfo); 
+					sem_post(&despertar); 		
+				}
+					
 			pthread_mutex_unlock(&elfos);
 			sem_wait(&esperando_santa_elf);
 
 			printf("recibiendo ayuda de santa \n");
 
 			pthread_mutex_lock(&elfos);
-				if(sem_trywait(&elf_con_dif) != 0){
+				if(sem_trywait(&elf_con_dif) != 0){ //si soy el ultimo
+
 					for(int i = 2; i>0; i--) {sem_post(&elf_con_dif);}	
 					for(int j = 3; j>0; j--) {sem_post(&otros_elfos);}
 				}
-				printf("recibí ayuda de santa !\n");
+				
 			pthread_mutex_unlock(&elfos);
-			printf("juguete creado !\n");
+			
 		}
+		printf("hacer juguete\n");
 	}	
 	pthread_exit(0);
 }
 
 
 void *reno(void *arg){
-	//sleep((int)(rand() % 10 + 1));
+	//sleep((int)(rand() % 10 + 1)); //simula el hecho de estar de vacaciones
 	pthread_mutex_lock(&renos);
 	
 		if( sem_trywait(&ren_vuelta_vacaciones) != 0) {//si es el último
+				sem_post(&despertar_reno);
 				sem_post(&despertar);
-				printf("llegue de vacaciones y soy el ultimo!\n");
+				printf("llegue de vacaciones y soy el ultimo! ------------\n");
 		}
-		else printf("llegue de vacaciones!\n");
+		else printf("llegue de vacaciones! --------------\n");
 	pthread_mutex_unlock(&renos);
-	//sem_wait(&esperando_en_cabaña);
+	sem_wait(&esperando_en_cabaña);
 	pthread_exit(0);
 }
 
 void *santa(void *arg){
 	do{
 	sem_wait(&despertar);
+	
 	pthread_mutex_lock(&renos);
-        if(sem_trywait(&ren_vuelta_vacaciones) == 0) //veo si estan los renos
-			sem_post(&ren_vuelta_vacaciones); //si no estan, devuelvo el turno que tome	
-		else{
-			printf("trineo hecho ! \n"); //si no hay lugares libres, preparo el trineo
-			sem_post(&ren_vuelta_vacaciones);
+        if(sem_trywait(&despertar_reno) == 0){ //veo si estan los renos
+			
+				printf("trineo hecho ! ------\n"); 
+				for(int i = 0; i <= CANT_RENO; i++) {
+					sem_post(&esperando_en_cabaña);
+				}
+		
 		}
 	pthread_mutex_unlock(&renos);
+    
     pthread_mutex_lock(&elfos);
-		if(sem_trywait(&elf_con_dif) == 0) //veo si hay elfos
-			sem_post(&elf_con_dif); //si no hay, devuelvo el turno que tome	
-		else{
+		if(sem_trywait(&despertar_elfo) == 0){ //veo si hay elfos
 			for(int j = 3; j>0; j--) {sem_post(&esperando_santa_elf); printf("ayudando a un elfo!\n");}
 			for(int i = 2; i>0; i--) {sem_post(&elf_con_dif);}	
 		}
@@ -94,6 +116,8 @@ int main(){
 	
 	
 	sem_init(&despertar,0,0);
+	sem_init(&despertar_reno,0,0);
+	sem_init(&despertar_elfo,0,0);
 	sem_init(&otros_elfos,0,3);
 	sem_init(&elf_con_dif ,0,2);
 	sem_init(&esperando_santa_elf,0,0);
@@ -105,23 +129,31 @@ int main(){
 	
 	pthread_create(&santa_claus,NULL,santa,NULL);
 	
-	for(int i = 0; i<CANT_ELFO; i++){
-	pthread_create(&elfos_a_usar[i],NULL,elfo,NULL);
+	for(int a = 0; a<CANT_ELFO; a++){
+	pthread_create(&elfos_a_usar[a],NULL,elfo,NULL);
 	}
-	for(int i = 0; i<CANT_RENO; i++){
-	pthread_create(&renos_a_usar[i],NULL,reno,NULL);
+	for(int b = 0; b<CANT_RENO; b++){
+	pthread_create(&renos_a_usar[b],NULL,reno,NULL);
 	}
 	
 
-	for(int i = 0; i<CANT_ELFO; i++){
-	pthread_join(elfos_a_usar[i],NULL);
+	for(int c = 0; c<CANT_ELFO; c++){
+	pthread_join(elfos_a_usar[c],NULL);
 	}
-	for(int i = 0; i<CANT_RENO; i++){
-	pthread_join(renos_a_usar[i],NULL);
+	for(int d = 0; d<CANT_RENO; d++){
+	pthread_join(renos_a_usar[d],NULL);
 	}
 	
 	pthread_cancel(santa_claus);
 	
+	sem_close(&despertar );
+	sem_close(&despertar_reno );
+	sem_close(&despertar_elfo);
+	sem_close(&otros_elfos );
+	sem_close(&elf_con_dif );
+	sem_close(&esperando_santa_elf );
+	sem_close(&esperando_en_cabaña );
+	sem_close(&ren_vuelta_vacaciones );
 
 
 	return 0;
